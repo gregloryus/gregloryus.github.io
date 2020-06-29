27998f024dd7727a6f3610faa07ebd81f4cbeb26
gregloryus <63573927+gregloryus@users.noreply.github.com>
2020-06-28 18:33:51 -0700

    Update simple-solo-2.js

--- simple-solo-2.js
+++ simple-solo-2.js
@@ -63,7 +63,7 @@ class Tracer {
     }
     if (roll === 11) {
       const reroll = random(100)
-      if (reroll < 99 && lines.length > 3 ) {
+      if (reroll < 99 && lines.length > 1 ) {
         lines.pop()
       }
       if (reroll < 1) {
