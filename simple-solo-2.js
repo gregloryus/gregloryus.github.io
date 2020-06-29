78c619e67f36a1533b34b577e18f6e22f13c1e5b
greg <g@g.com>
2020-06-28 19:43:02 -0700

    longer hue

--- simple-solo-2.js
+++ simple-solo-2.js
@@ -15,7 +15,7 @@ function setup() {
 
 function draw() {
   colorMode(HSB, 100);
-  let hue = ((frameCount/2) % 100);
+  let hue = ((frameCount/10) % 100);
   let saturation = lines.length*5;
   let brightness = 100;
   let c = color(hue, saturation, brightness);
