f76393b49457a730cc3ae13aec460853b7cbda35
gregloryus <63573927+gregloryus@users.noreply.github.com>
2020-07-02 14:30:17 -0700

    Update cactus-supernova.js

--- cactus-supernova.js
+++ cactus-supernova.js
@@ -83,7 +83,7 @@ class Tracer {
     if (reroll < 4) {
       const newTracer = new Tracer(this.y, this.x);
         lines.push(newTracer)
-        if (lines.length > 10000) {
+        if (lines.length > 1000 * 1000/vw) {
           lines = [];
           background(0);
           lines.push(newTracer)
