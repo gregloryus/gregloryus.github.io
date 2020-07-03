40ab3af922149c1b399509f440e0a99e2ee6a0a9
gregloryus <63573927+gregloryus@users.noreply.github.com>
2020-07-03 13:22:38 -0700

    Update simple-solo.js

--- simple-solo.js
+++ simple-solo.js
@@ -1,8 +1,14 @@
 let x;
 let y;
 
-const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
-const vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0);
+const vw = Math.max(
+  document.documentElement.clientWidth || 0,
+  window.innerWidth || 0
+);
+const vh = Math.max(
+  document.documentElement.clientHeight || 0,
+  window.innerHeight || 0
+);
 
 let lines = [];
 
@@ -11,68 +17,73 @@ function setup() {
   background(0);
   let line1 = new Tracer();
   lines.push(line1);
+  noLoop();
+  setInterval(redraw, 0); // where 10 is the minimum time between frames in ms
 }
 
 function draw() {
   colorMode(HSB, 100);
-  let hue = ((frameCount/2) % 100);
-  let saturation = lines.length*5;
+  let hue = (frameCount / 2) % 100;
+  let saturation = lines.length * 5;
   let brightness = 100;
   let c = color(hue, saturation, brightness);
   const r = floor(random(4));
-  let strokeW = 1
+  let strokeW = 1;
   stroke(c);
   strokeWeight(1);
 
-  frameRate()
+  frameRate();
 
   for (line of lines) {
     line.move();
-    line.display()
+    line.display();
   }
-
 }
 
 class Tracer {
-  constructor(y = height, x = width/2) {
-    this.x = x
-    this.y = y
+  constructor(y = height, x = width / 2) {
+    this.x = x;
+    this.y = y;
   }
   move() {
     let z = 1;
-    // let xDistance = random((100 / lines.length)/2) 
+    // let xDistance = random((100 / lines.length)/2)
     // let yDistance = random((100 / lines.length)/2)
-    let xDistance = z
-    let yDistance = z
-    const roll = floor(random(1,12));
-    if (roll === 1 || roll === 2) { // ++ Right-Down
-      this.x = this.x + xDistance
-      this.y = this.y + yDistance
+    let xDistance = z;
+    let yDistance = z;
+    const roll = floor(random(1, 12));
+    if (roll === 1 || roll === 2) {
+      // ++ Right-Down
+      this.x = this.x + xDistance;
+      this.y = this.y + yDistance;
     }
-    if (roll === 3 || roll === 4 || roll == 5) { // -- Left-Up
-      this.x = this.x - xDistance 
-      this.y = this.y - yDistance
+    if (roll === 3 || roll === 4 || roll == 5) {
+      // -- Left-Up
+      this.x = this.x - xDistance;
+      this.y = this.y - yDistance;
     }
-    if (roll === 6 || roll === 7 || roll === 8) { // +- Right-Up
-      this.x = this.x + xDistance 
-      this.y = this.y - yDistance
+    if (roll === 6 || roll === 7 || roll === 8) {
+      // +- Right-Up
+      this.x = this.x + xDistance;
+      this.y = this.y - yDistance;
     }
-    if (roll === 9 || roll === 10) { // -+ Left-Down
-      this.x = this.x - xDistance 
-      this.y = this.y + yDistance
+    if (roll === 9 || roll === 10) {
+      // -+ Left-Down
+      this.x = this.x - xDistance;
+      this.y = this.y + yDistance;
     }
     if (roll === 11) {
-      const reroll = random(100)
+      const reroll = random(100);
       if (reroll < 1) {
         const newTracer = new Tracer(this.y, this.x);
-        lines.push(newTracer)
-        let strokeW = 1
-        console.log("lines: " + lines.length)
-        console.log("stroke: " + strokeW)
+        lines.push(newTracer);
+        let strokeW = 1;
+        console.log("lines: " + lines.length);
+        console.log("stroke: " + strokeW);
         if (lines.length > 1000) {
           lines = [];
           background(0);
-          lines.push(newTracer)
+          lines.push(newTracer);
         }
       }
     }
@@ -83,14 +94,13 @@ class Tracer {
       this.y = height;
     }
     if (this.x > width) {
-      this.x = 0
+      this.x = 0;
     }
     if (this.x < 0) {
       this.x = width;
-  }
-
+    }
   }
   display() {
-    point(this.x, this.y)
+    point(this.x, this.y);
   }
 }
