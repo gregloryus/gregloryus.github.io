be670f72d2225110aa243e11e7f0f5632f2eb956
gregloryus <63573927+gregloryus@users.noreply.github.com>
2021-04-09 02:54:03 -0700

    fix

--- touchControls.js
+++ touchControls.js
@@ -1,32 +1,35 @@
 // // function for touch controls
-// function mouseDragged() {
-//   let perceptionRadius = touchRadius; // how far it can detect
-//   let perceptionCount = 100; // how many items it can perceive at once
+function mouseDragged() {
+  let perceptionRadius = touchRadius; // how far it can detect
+  let perceptionCount = 100; // how many items it can perceive at once
 
-//   //capturing mouse locations
-//   let mouseV = createVector(mouseX, mouseY);
-//   let pmouseV = createVector(pmouseX, pmouseY);
+  //capturing mouse locations
+  let mouseV = createVector(mouseX, mouseY);
+  let pmouseV = createVector(pmouseX, pmouseY);
 
-//   //sets a drag force that gets added to particle velocity
-//   let dragForce = p5.Vector.sub(mouseV, pmouseV);
-//   dragForce.mult(0.2);
+  //sets a drag force that gets added to particle velocity
+  let dragForce = p5.Vector.sub(mouseV, pmouseV);
+  dragForce.mult(0.2);
 
-//   for (const other of quadTree.getItemsInRadius(
-//     mouseX,
-//     mouseY,
-//     perceptionRadius,
-//     perceptionCount
-//   )) {
-//     //distance between mouse and particle
-//     let d = dist(mouseX, mouseY, other.pos.x, other.pos.y);
+  for (const other of quadTree.getItemsInRadius(
+    mouseX,
+    mouseY,
+    perceptionRadius,
+    perceptionCount
+  )) {
+    if (!other.water) {
+      return;
+    }
+    //distance between mouse and particle
+    let d = dist(mouseX, mouseY, other.pos.x, other.pos.y);
 
-//     //heat and push particles, increasing magnitude the closer you are
-//     other.temp = other.temp + (100 - d) / 10;
-//     other.acc.add(dragForce);
-//     other.acc.mult((100 - d) / 100);
-//     other.acc.limit(100);
-//   }
-// }
+    //heat and push particles, increasing magnitude the closer you are
+    other.temp = other.temp + (100 - d) / 10;
+    other.acc.add(dragForce);
+    other.acc.mult((100 - d) / 100);
+    other.acc.limit(100);
+  }
+}
 
 // MAKE A NEW PLANT
 //   let plant = new Plant(mouseX, mouseY);
@@ -41,6 +44,6 @@
 //   lines[0].pos.y = mouseY;
 // }
 
-function doubleClicked() {
-  window.location.reload();
-}
+// function doubleClicked() {
+//   // window.location.reload();
+// }
