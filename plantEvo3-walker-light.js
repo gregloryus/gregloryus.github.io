5b0839a90e04d37198bd0b41e658cf3fd69882c0
greg <g@g.com>
2021-04-26 02:36:21 -0700

    test

--- plantEvo3-walker-light.js
+++ plantEvo3-walker-light.js
@@ -8,7 +8,7 @@ let sunMoveMode = true; // is the sun moving? if false, sun is fixed
 
 //Amount sliders
 let numOfLight = 100; // number of light particles
-let lightSize = 3;
+let lightSize = 1;
 let lightOpacity = 100; //opacity of light (20 min)
 let lightSpeed = 2; // speed of light
 let lightPulseRate = 400; // how quickly light pulses (400 = chill)
