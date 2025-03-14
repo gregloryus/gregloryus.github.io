2898b9f7adb5129213e3b0f548b85244062cf620
gregloryus <63573927+gregloryus@users.noreply.github.com>
2025-03-13 23:49:05 -0500

    Update treemap.js

--- treemap.js
+++ treemap.js
@@ -15,10 +15,11 @@ function initMap() {
   map = L.map("map").setView([42.0451, -87.6877], 14);
 
   // Add OpenStreetMap tile layer
-  L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-    maxZoom: 19,
+  L.tileLayer("https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png", {
     attribution:
-      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+    subdomains: "abcd",
+    maxZoom: 19,
   }).addTo(map);
 
   // Initialize marker cluster group with improved settings for precision
